83eb289b31dc586cd3d0c04f378c88ae4368418d
Gabriele Preziosa <preziosagabriele2002@gmail.com>
2018-01-08 20:12:34 +0100

    gg

--- loadlogin.php
+++ loadlogin.php
@@ -4,22 +4,22 @@
 	
 	if (isset($_POST["email"]) && isset($_POST["passwd"]) ) {
 	
-		$checkemail = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $email . "'");
+		$checkemail = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $_POST["email"] . "'");
 	
 		if ($checkemail->num_rows > 0) {
 	
-			$checkpass = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $email . "' AND PASSWORD = '" . md5($passwd) . "'");
+			$checkpass = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $_POST["email"] . "' AND PASSWORD = '" . md5($_POST["passwd"]) . "'");
 	
 			if ($checkpass->num_rows > 0) {
 	
 				$userdata = $checkpass->fetch_assoc();
 	
 				//save userdata in session variables
-				$_SESSION["user_email"] = $rows["EMAIL"];
-				$_SESSION["user_ruolo"] = $rows["RUOLO"];
-				$_SESSION["user_name"] = $rows["COGNOME"] . " " . $rows["NOME"];
+				$_SESSION["user_email"] = $userdata["EMAIL"];
+				$_SESSION["user_ruolo"] = $userdata["RUOLO"];
+				$_SESSION["user_name"] = $userdata["COGNOME"] . " " . $userdata["NOME"];
 				
-				header("location: home.php");
+				header("location: index.php");
 	
 			}   else {
 				
@@ -33,6 +33,10 @@
 	
 		}
 	
+	}	else {
+
+		 echo "Suck";
+
 	}
 
  ?>
