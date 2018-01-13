bbcd45eb7629a44ee1c749fecfa3881c9d71da47
gesugegiu <32303608+gesugegiu@users.noreply.github.com>
2018-01-13 17:51:55 +0100

    A big update :D

--- get.php
+++ get.php
@@ -15,13 +15,40 @@ function getRuolo($email) {
 function getCognomeNome($email) {
 
     require "mysql.php";
+	$ret = "";
+	
+    $sql = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $email . "'");
+    if($sql->num_rows){
+		
+		$data = $sql->fetch_assoc();
 
-    $execute = $mysqli->query("SELECT * FROM users WHERE EMAIL = '" . $email . "'");
-    
-    $data = $execute->fetch_assoc();
+		$ret = $data["COGNOME"] . " " .$data["NOME"] ;
+	}else{
+		$ret = "Errore";
+	}
+	
+	return $ret;
+}
 
-    return $data["COGNOME"] . " " .$data["NOME"] ;
+function isAccept($id){
+	require "mysql.php";
+	$sql = $mysqli->query("SELECT * FROM commissioni WHERE id = '" . $id . "'");
+    if($sql->num_rows){
+		
+		$row = $sql->fetch_assoc();
 
+		if($row['ACCETTATO'] == 'Si'){
+			$ret = 'Accettato';
+		}else if($row['ACCETTATO'] == 'No'){
+			$ret = 'Non accettato';
+		}else{
+			$ret = 'Errore';
+		}
+	}else{
+		$ret = "Errore";
+	}
+	
+	return $ret;
 }
 
 ?>
--- mysql.php
+++ mysql.php
@@ -9,19 +9,35 @@ if ($mysqli->connect_error) {
 
 /*DEFAULT TABLE*/
 $mysqli->query("CREATE TABLE IF NOT EXISTS users (EMAIL VARCHAR(40), PASSWORD VARCHAR(50), NOME VARCHAR(20), COGNOME VARCHAR(20), RUOLO VARCHAR(15), LASTIP VARCHAR(15))");
+$mysqli->query("
+  CREATE TABLE `commissioni` IF NOT EXISTS(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `EMAIL_CLIENTE` varchar(40) NOT NULL,
+  `EMAIL_TECNICO` varchar(40) NOT NULL,
+  `ACCETTATO` varchar(2) NOT NULL,
+  `INIZIATO` varchar(2) NOT NULL,
+  `CATEGORIA` varchar(20) NOT NULL,
+  `SERVIZIO` varchar(512) NOT NULL,
+  `DESCRIZIONE` longtext NOT NULL,
+  `NOTE` longtext NOT NULL,
+  `BUDGET` varchar(7) NOT NULL,
+  `PREZZO_FINALE` varchar(7) NOT NULL,
+  `DATA_INIZIO` varchar(10) NOT NULL,
+  `DATA_FINE` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1	
+");
+$mysqli->query("
+  CREATE TABLE `tecnici` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `COGNOME` varchar(20) NOT NULL,
+  `NOME` varchar(20) NOT NULL,
+  `RUOLO` varchar(20) NOT NULL,
+  `EMAIL` varchar(40) NOT NULL,
+  `DATA_ASSUNZIONE` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+  ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
 
-$mysqli->query("CREATE TABLE `commissioni` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `EMAIL_CLIENTE` varchar(40) NOT NULL,
- `EMAIL_TECNICO` varchar(40) NOT NULL,
- `CATEGORIA` varchar(20) NOT NULL,
- `SERVIZIO` varchar(512) NOT NULL,
- `DESCRIZIONE` longtext NOT NULL,
- `NOTE` longtext NOT NULL,
- `BUDGET` varchar(7) NOT NULL,
- `DATA_INIZIO` varchar(10) NOT NULL,
- `DATA_FINE` varchar(10) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1");
+");
 
 ?>
